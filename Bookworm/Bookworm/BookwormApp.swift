515d1c293cb13c5dd4374ce281ef8735642cda17
Bruce Wang <brucewang7777@gmail.com>
2024-01-04 15:51:02 +0800

    Create an empty project Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Bruce Wang on 2024/1/4.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Bruce Wang on 2024/1/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
