09f35f89a923371f48c8aa6e6326032cd4d286f9
Bruce Wang <brucewang7777@gmail.com>
2024-01-04 18:22:39 +0800

    Introduction to SwiftData

--- Bookworm/Bookworm/BookwormApp.swift
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -13,5 +13,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Student.self)
     }
 }
--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -5,17 +5,34 @@
 //  Created by Bruce Wang on 2024/1/4.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(students) { student in
+                    Text("\(student.firstName) \(student.lastName)")
+                }
+            }
+            .navigationTitle("Students")
+            .toolbar {
+                Button(action: {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                            let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                            let chosenFirstName = firstNames.randomElement()!
+                            let chosenLastName = lastNames.randomElement()!
+                    modelContext.insert(Student(id: UUID(), firstName: chosenFirstName, lastName: chosenLastName))
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
         }
-        .padding()
     }
 }
 
